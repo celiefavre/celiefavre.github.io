25cad20ea4ffa67df1cd1654af0f6f13d4e86b40
Nicolas Devaux <ndevaux@gmail.com>
2015-08-07 11:57:12 +0200

    refactor

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//TweenLite.from("#test", 1, {
+//    drawSVG: "20% 20%",
+//    delay: 1
+//});
+
+
+TweenMax.from(
+    $(".chart-stroke"), 6, {
+        drawSVG: "0%",
+        ease:Power1.easeInOut
+    });
+
